53414e8f41e5d2424ab4118d104d3236639721b8
Alessandro Conigli <alessandroconigli@gmail.com>
2025-03-19 10:00:28 +0100

    normalise_data and rescale_data takes now additional parameters a,b for [a,b] rescaling

--- src/Data/DataProcessing.jl
+++ src/Data/DataProcessing.jl
@@ -1,11 +1,14 @@
 @doc raw"""
-    normalise_data(x; dims=ndims(x))
+    normalise_data(x; dims=ndims(x), a=nothing,b=nothing)
 
-Normalise data to mean zero and standard deviation one.
-By default it is applied to the last  input dimension 
-
+Normalise x to mean zero and standard deviation one.
+If a and b are given, x is rescaled in [a,b] instead.
+By default it is applied to the last input dimension. 
 """
-function normalise_data(x; dims=ndims(x))
+function normalise_data(x; dims=ndims(x), a=nothing,b=nothing)
+    if !isnothing(a) && !isnothing(b)
+        return normalise_data_a_b(x, a, b; dims=dims)
+    end
     μ = mean(x; dims=dims)
     σ = std(x; dims=dims)
     z = (x .- μ) ./ σ
@@ -13,15 +16,33 @@ function normalise_data(x; dims=ndims(x))
 end
 
 @doc raw"""
-    normalise_data_a_b(x; dims=ndims(x))
+    normalise_data_a_b(x, a, b; dims=ndims(x))
 
-Normalise data to mean zero and standard deviation one.
-By default it is applied to the last  input dimension 
-
+Rescale x in [a,b].
+By default it is applied to the last input dimension. 
 """
-function normalise_data_a_b(x; dims=ndims(x), a=0.0,b=1.0)
+function normalise_data_a_b(x,a,b; dims=ndims(x))
     z = (b-a) .* (x .- minimum(x,dims=dims)) ./ ( maximum(x,dims=dims) .- minimum(x,dims=dims)) .+ a  
     return z, minimum(x,dims=dims), maximum(x,dims=dims)
+end
+
+@doc raw"""
+    rescale_data(z, p1, p2;  a=nothing,b=nothing)
+
+Rescale normally distributed z to mean p1 and standard deviation p2
+If a and b are given, Rescale z from [a,b] to min p1 and maxi p2.
+By default it is applied to the last input dimension. 
+"""
+function rescale_data(z, p1, p2; a=nothing,b=nothing)
+    if !isnothing(a) && !isnothing(b)
+        return rescale_data_a_b(x, p1, p2; a=a,b=b)
+    end
+    x = z .* p2 .+ p1
+    return x
+end
+function rescale_data_a_b(z, minval, maxval; a=0.0, b=1.0)
+    x = (z .- a) .* (maxval .- minval) ./ (b -a) .+ minval
+    return x
 end
 
 @doc raw"""
@@ -29,10 +50,9 @@ end
 
 Given a dictionary of DataFrames df_dict, a starting date in_date and 
 a final date fin_date, this function returns an array with normalised data for entry in the dictionary.
-It also returns a dictionary containing the normalisation parameters (mean and std) necessary to scale back the data.
-Currently data are normalised with mean zero and standard deviation one.
-TO IMPLEMENT: option of givin a and b to normalise data between a and b extrema.
-
+It also returns a dictionary containing the normalisation parameters mean and std  to scale back the data.
+By default data are normalised with mean zero and standard deviation one.
+If a and b are given, data are rescaled in [a,b] and the returned dictionary contains the min and max values to scale back the data. 
 """
 function get_processed_data(df_dict::Dict, in_date::Date, fin_date::Date; a=nothing, b=nothing)
     
@@ -40,27 +60,14 @@ function get_processed_data(df_dict::Dict, in_date::Date, fin_date::Date; a=noth
     ydata = Array{Float64}(undef, length(df_dict), tlen)
     norm_param = Dict()
     
-    for (idx, k) in enumerate(keys(df_dict))
+    for (idx, k) in enumerate(sort(collect(keys(df_dict))))
         filter!(x -> in_date <= x.date <= fin_date, df_dict[k])
-        if isnothing(a) && isnothing(b)
-            y, ymean, ystd = normalise_data(df_dict[k].cases)
-            norm_param[k] = (ymean[1], ystd[1])
-            ydata[idx,:] = y
-        else
-            y, minval, maxval = normalise_data_a_b(df_dict[k].cases,a=a,b=b)
-            norm_param[k] = (minval[1], maxval[1])
-            ydata[idx,:] = y
-        end
+
+        y, p1, p2 = normalise_data(df_dict[k].cases, a=a,b=b)
+        norm_param[k] = (p1[1], p2[1])
+        ydata[idx,:] = y
     end 
     return  ydata, norm_param
 end
 
-function rescale_data(z, mean_, std_)
-    x = z .* std_ .+ mean_
-    return x
-end
 
-function rescale_data_a_b(z, minval, maxval; a=0.0, b=1.0)
-    x = (z .- a) .* (maxval .- minval) ./ (b -a) .+ minval
-    return x
-end
